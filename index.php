cba92cfbded096e9cb1afb2308c8e725115fd1e2
Scarlett <nathanchick@Scarletts-MacBook-Pro.local>
2022-07-08 13:56:37 +0100

    pizza choice form-updated

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
-<!-- creates a checkbox -->
-<input type="checkbox" name="checkbox_name" value="checkox_value">
+<?php
 
-<!-- this checks if a checkbox has been ticked -->
-if(filter_has_var(INPUT_POST,'checkbox_name')) {
-  // ...
+session_start();
+
+require 'inc/header.php';
+
+require 'inc/functions.php';
+
+$pizza_toppings = [
+	'pepperoni' => 0.5,
+	'mushrooms' => 1,
+	'onions' => 1.5,
+	'sausage' => 2.5,
+	'bacon' => 1.0,
+];
+
+$request_method = $_SERVER['REQUEST_METHOD'];
+
+if ($request_method === 'GET') {
+	require 'inc/get.php';
+} elseif ($request_method === 'POST') {
+	require 'inc/post.php';
 }
 
-<!-- a checkbox has no label so you should always use a checkbox with a label feature -->
-<input type="checkbox" name="agree" id="agree">
-<label for="agree">I agree</label>
-
-<!-- this is an alternative way to use the label-->
-<label>
-    <input type="checkbox" name="agree"> I agree
-</label>
-
-<!-- When php sees the square brackets ([]) in the field name, it’ll create an associative array of values where the key is the checkbox’s name and the values are the selected values. -->
-<form action="index.php" method="post">
-	<input type="checkbox" name="colors[]" value="red" id="color_red" />
-	<label for="color_red">Red</label>
-
-	<input type="checkbox" name="colors[]" value="green" id="color_green" />
-	<label for="color_red">Green</label>
-
-	<input type="checkbox" name="colors[]" value="blue" id="color_blue" />
-	<label for="color_red">Blue</label>
-        <input type="submit" value="Submit">
-</form>
+require 'inc/footer.php';
