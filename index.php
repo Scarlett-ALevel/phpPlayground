35b659cc3d32d95f2fa5ea1cd352f18ad3e62159
Scarlett <nathanchick@Scarletts-MacBook-Pro.local>
2022-07-08 14:30:55 +0100

    classes-OOP

--- index.php
+++ index.php
@@ -1,25 +1,60 @@
 <?php
+// CLASSES IN OOP
+class BankAccount
+{
+}
+// defining a class called BankAccount
 
-session_start();
+// defining a method within a class
+class ClassName
+{
+	public function methodName(parameter_list)
+	{
+		// implementation
+	}
+}
+// Like a property, a method also has one of the three visibility modifiers: public, private, and protected. If you define a method without any visibility modifier, it defaults to public.
 
-require 'inc/header.php';
+// In PHP, $this keyword references the current object of the class. The $this keyword allows you to access the properties and methods of the current object within the class using the object operator (->)
 
-require 'inc/functions.php';
+class BankAccount
+{
+	public $accountNumber;
 
-$pizza_toppings = [
-	'pepperoni' => 0.5,
-	'mushrooms' => 1,
-	'onions' => 1.5,
-	'sausage' => 2.5,
-	'bacon' => 1.0,
-];
+	public $balance;
 
-$request_method = $_SERVER['REQUEST_METHOD'];
+	public function deposit($amount)
+	{
+		if ($amount > 0) {
+			$this->balance += $amount;
+		}
+	}
 
-if ($request_method === 'GET') {
-	require 'inc/get.php';
-} elseif ($request_method === 'POST') {
-	require 'inc/post.php';
+
+
+	public function withdraw($amount)
+	{
+		if ($amount <= $this->balance) {
+			$this->balance -= $amount;
+			return true;
+		}
+                return false;
+
+	}
+
 }
 
-require 'inc/footer.php';
+// this is called method chaining
+$account->deposit(100)
+	->deposit(200)
+	->deposit(300);
+
+
+/*
+types of access:
+Use the public access modifier to allow access to properties and methods from both inside and outside of the class.
+Use the private access modifier to prevent access from the outside of the class.
+Do use private properties with a pair of public getter/setter methods.
+*/
+
+// there is a constructor and destructor method which can be used to help reduce the complexity of code.
