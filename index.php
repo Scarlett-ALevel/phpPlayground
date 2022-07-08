832ed0c1a11b088e0b5440cfd185d51920e82839
Scarlett <nathanchick@Scarletts-MacBook-Pro.local>
2022-07-08 09:04:08 +0100

    variables

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP Syntax</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Variables</title>
 </head>
 <body>
-        <h1><?php echo 'PHP Syntax'; ?></h1>
+    <?php
+        $title = 'PHP is awesome!';
+    ?>
+    <h1><?php echo $title; ?></h1>
 </body>
 </html>
