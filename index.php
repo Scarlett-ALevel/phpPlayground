11bd86056b2e38cb56b3659625d3d419d92508ee
Scarlett <nathanchick@Scarletts-MacBook-Pro.local>
2022-07-08 13:39:41 +0100

    checkoxes

--- index.php
+++ index.php
@@ -1,31 +1,29 @@
-<!DOCTYPE html>
-<!-- form to subscribe to an email service -->
+<!-- creates a checkbox -->
+<input type="checkbox" name="checkbox_name" value="checkox_value">
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css">
-    <title>PHP Form Demo</title>
-</head>
-<body>
-    <main>
-        <form action="subscribe.php" method="post">
-            <div>
-                <label for="name">Name:</label>
-                <input type="text" name="name" required="required" placeholder="Enter your name" />
-            </div>
+<!-- this checks if a checkbox has been ticked -->
+if(filter_has_var(INPUT_POST,'checkbox_name')) {
+  // ...
+}
 
-            <div>
-                <label for="name">Email:</label>
-                <input type="email" name="email" required="required" placeholder="Enter your email" />
-            </div>
+<!-- a checkbox has no label so you should always use a checkbox with a label feature -->
+<input type="checkbox" name="agree" id="agree">
+<label for="agree">I agree</label>
 
-            <button type="submit">Subscribe</button>
-        </form>
-    </main>
-</body>
-</html>
+<!-- this is an alternative way to use the label-->
+<label>
+    <input type="checkbox" name="agree"> I agree
+</label>
 
-<script>alert('ALERT EXAMPLE');</script>
-<!-- pages an alert -->
+<!-- When php sees the square brackets ([]) in the field name, it’ll create an associative array of values where the key is the checkbox’s name and the values are the selected values. -->
+<form action="index.php" method="post">
+	<input type="checkbox" name="colors[]" value="red" id="color_red" />
+	<label for="color_red">Red</label>
+
+	<input type="checkbox" name="colors[]" value="green" id="color_green" />
+	<label for="color_red">Green</label>
+
+	<input type="checkbox" name="colors[]" value="blue" id="color_blue" />
+	<label for="color_red">Blue</label>
+        <input type="submit" value="Submit">
+</form>
